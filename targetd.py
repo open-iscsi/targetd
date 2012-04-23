54a031ba1ae11b7ccdd5eddf3205849fa1d36623
Andy Grover <agrover@redhat.com>
2012-04-23 14:53:02 -0700

    switch to symmetric-jsonrpc framework
    Copy example code from symmetric-jsonrpc.
    
    use setproctitle module to set proc name
    
    add GPL3+ license header, must use GPL3 since we will be using AGPLv3
    code (rtslib). This combination is expressly allowed by AGPLv3 clause 13.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- targetd.py
+++ targetd.py
@@ -1,12 +1,82 @@
-from twisted.internet import protocol, reactor
+#!/usr/bin/env python
 
-class Echo(protocol.Protocol):
-    def dataReceived(self, data):
-        self.transport.write(data)
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Based upon example code from python-symmetric-jsonrpc:
+# Copyright (C) 2009 Egil Moeller <redhog@redhog.org>
+# Copyright (C) 2009 Nicklas Lindgren <nili@gulmohar.se>
+#
+# Copyright 2012, Andy Grover <agrover@redhat.com>
+#
+# A server that exposes a network interface for the LIO
+# kernel target.
 
-class EchoFactory(protocol.Factory):
-    def buildProtocol(self, addr):
-        return Echo()
+import symmetricjsonrpc, sys
+import setproctitle
 
-reactor.listenTCP(1234, EchoFactory())
-reactor.run()
+setproctitle.setproctitle("targetd")
+
+class TargetRPCServer(symmetricjsonrpc.RPCServer):
+    class InboundConnection(symmetricjsonrpc.RPCServer.InboundConnection):
+        class Thread(symmetricjsonrpc.RPCServer.InboundConnection.Thread):
+            class Request(symmetricjsonrpc.RPCServer.InboundConnection.Thread.Request):
+                def dispatch_notification(self, subject):
+                    print "dispatch_notification(%s)" % (repr(subject),)
+                    assert subject['method'] == "shutdown"
+                    # Shutdown the server. Note: We must use a
+                    # notification, not a method for this - when the
+                    # server's dead, there's no way to inform the
+                    # client that it is...
+                    self.parent.parent.parent.shutdown()
+
+                def dispatch_request(self, subject):
+                    print "dispatch_request(%s)" % (repr(subject),)
+                    assert subject['method'] == "ping"
+                    # Call the client back
+                    # self.parent is a symmetricjsonrpc.RPCClient subclass (see the client code for more examples)
+                    res = self.parent.request("pingping", wait_for_response=True)
+                    print "parent.pingping => %s" % (repr(res),)
+                    assert res == "pingpong"
+                    return "pong"
+
+if '--help' in sys.argv:
+    print """client.py
+    --ssl
+        Encrypt communication with SSL using M2Crypto. Requires a
+        server.pem and server.key in the current directory.
+"""
+    sys.exit(0)
+
+if '--ssl' in sys.argv:
+    # Set up a SSL socket
+    import M2Crypto
+    ctx = M2Crypto.SSL.Context()
+    ctx.load_cert('server.pem', 'server.key')
+    s = M2Crypto.SSL.Connection(ctx)
+else:
+    # Set up a TCP socket
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+#  Start listening on the socket for connections
+s.bind(('', 4712))
+s.listen(1)
+
+# Create a server thread handling incoming connections
+server = TargetRPCServer(s, name="targetd")
+
+# Wait for the server to stop serving clients
+server.join()
+
