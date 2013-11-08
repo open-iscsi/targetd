453029b48265ee27c2af8141cb8867db0ee919dc
Andy Grover <agrover@redhat.com>
2013-11-08 14:56:02 -0800

    update to 0.7
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'targetd',
-    version = '0.6.1',
+    version = '0.7',
     description = 'Linux remote storage API daemon',
     license = 'GPLv3',
     maintainer = 'Andy Grover',
