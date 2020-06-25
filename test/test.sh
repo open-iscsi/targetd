b50489dde981853f966f8895c089080324566ba5
Tony Asleson <tasleson@redhat.com>
2020-06-25 11:40:48 -0500

    travis-ci: Add basic testing
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

---
+++ test/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir -p /etc/target || exit 1
+cp test/targetd.yaml /etc/target/ || exit 1
+
+# We don't place the password into the example yaml to prevent
+# people from using it and having it contain a bad password by default
+echo "password: targetd" >> /etc/target/targetd.yaml
+
+# Create the needed block devices for lvm, btrfs, and zfs for testing
+truncate -s 1T /tmp/block1.img || exit 1
+truncate -s 1T /tmp/block2.img || exit 1
+truncate -s 1T /tmp/block3.img || exit 1
+
+loop1=$(losetup -f --show /tmp/block1.img)
+loop2=$(losetup -f --show /tmp/block2.img)
+loop3=$(losetup -f --show /tmp/block3.img)
+
+# Create lvm needed parts
+vgcreate vg-targetd $loop1 || exit 1
+lvcreate -L 500G -T vg-targetd/thin_pool || exit 1
+
+# Create btrfs
+mkfs.btrfs $loop2 || exit 1
+mkdir -p /mnt/btrfs || exit 1
+mount $loop2 /mnt/btrfs || exit 1
+
+export PYTHONPATH=$(pwd)
+python3 scripts/targetd > /tmp/targetd.log 2>&1 &
+
+sleep 5 || exit 1
+echo "Dumping targetd output ..."
+cat /tmp/targetd.log
+
+# Run the actual tests, these need work ...
+echo "Running client test ..."
+python3 client || exit 1
+exit 0
