58a6242402f40ffd51d46f5e958a41d7f8e59905
Tony Asleson <tasleson@redhat.com>
2020-08-20 10:09:57 -0500

    test.sh: Add sample export file for testing parse
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- test/test.sh
+++ test/test.sh
@@ -85,11 +85,16 @@ zpool create zfs_targetd $loop3 || clean_up 1
 zfs create zfs_targetd/block_pool || clean_up 1
 zfs create zfs_targetd/fs_pool || clean_up 1
 
+# setup a sample nfs export file for parsing
+cp test/targetd_test_parse /tmp/. || clean_up 1
+export BASE_NFS_MOUNT="/mnt/nfs_mounts"
+
 if [ $SETUP -eq 1  ]; then
     exit 0
 fi
 
 export PYTHONPATH=$(pwd)
+export TARGETD_NFS_EXPORT="/tmp/targetd_test_parse"
 python3-coverage run scripts/targetd > /tmp/targetd.log 2>&1 &
 
 sleep 5 || clean_up 1
